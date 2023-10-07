47fbd8a4a358d20100882dec41346d17002a7355
Ning Kuang <17879507+alexkuang0@users.noreply.github.com>
2023-10-07 17:23:10 -0500

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# FastAPI Custom Response Header Doc Demo
+
+This repo demonstrates
+
+## How to run
+
+```sh
+python3 -m venv venv
+source ./venv/bin/activate
+pip install -r requirements.txt
+
+uvicorn main:app --reload
+```
+
+To check the generated docs, go to http://127.0.0.1:8000/docs#/default/demo_demo_get
---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, Response, status
+from random import randint
+
+app = FastAPI()
+
+VENDOR_CUSTOM_RESP_HEADERS = {
+    'X-Vendor-ErrorCode': {
+        'description': 'Error code defined by Vendor',
+        'schema': { 'type': 'number' }
+    }
+}
+
+@app.get('/demo', responses={
+    200: { 'headers': VENDOR_CUSTOM_RESP_HEADERS },
+    400: { 'headers': VENDOR_CUSTOM_RESP_HEADERS },
+})
+def demo(response: Response):
+    is_success = randint(0, 1)
+    if is_success:
+        response.headers['X-Vendor-ErrorCode'] = '0'
+        return { 'message': 'good' }
+    else:
+        response.headers['X-Vendor-ErrorCode'] = '0'
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return { 'message': 'bad' }
