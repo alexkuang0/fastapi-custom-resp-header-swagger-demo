2764fcbf49efc3b81529f3f71be5c0a658d2aa98
Ning Kuang <17879507+alexkuang0@users.noreply.github.com>
2023-10-07 20:39:45 -0500

    more status

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, Response, status, HTTPException
+from pydantic import BaseModel
 from random import randint
+
+class DemoResponse(BaseModel):
+    message: str
 
 app = FastAPI()
 
 VENDOR_CUSTOM_RESP_HEADERS = {
     'X-Vendor-ErrorCode': {
-        'description': 'Error code defined by Vendor',
+        'description': '0 means normal, 1 means error.',
         'schema': { 'type': 'number' }
     }
 }
@@ -13,13 +17,24 @@ VENDOR_CUSTOM_RESP_HEADERS = {
 @app.get('/demo', responses={
     200: { 'headers': VENDOR_CUSTOM_RESP_HEADERS },
     400: { 'headers': VENDOR_CUSTOM_RESP_HEADERS },
+    500: { 'headers': VENDOR_CUSTOM_RESP_HEADERS },
 })
-def demo(response: Response):
-    is_success = randint(0, 1)
-    if is_success:
-        response.headers['X-Vendor-ErrorCode'] = '0'
-        return { 'message': 'good' }
-    else:
-        response.headers['X-Vendor-ErrorCode'] = '1'
-        response.status_code = status.HTTP_400_BAD_REQUEST
-        return { 'message': 'bad' }
+def demo(response: Response) -> DemoResponse:
+    # randomly response with 200, 400 or 500
+    status = randint(0, 2)
+
+    match status:
+        case 0:
+            response.headers['X-Vendor-ErrorCode'] = '0'
+            return DemoResponse(message='good')
+        case 1:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                headers={ 'X-Vendor-ErrorCode': '1' },
+            )
+        case 2:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail={ 'request_id': '095e5265-1788-4a3e-a94f-67a09d021933' },
+                headers={ 'X-Vendor-ErrorCode': '2' },
+            )
